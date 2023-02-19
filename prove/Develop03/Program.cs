90cfdffca539b544c00a8c3636ecedc88243b775
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-18 19:07:51 -0700

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Linq;
 
-class Program
+namespace ScriptureApp
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main(string[] args)
+        {
+            Scripture scripture = new Scripture("2 Nephi 2:27", "And it must needs be that the devil should tempt the children of men, or they must become devilish.");
+
+            scripture.Display();
+            while (scripture.HasHiddenWords())
+            {
+                Console.WriteLine("Press enter to hide more words or type 'quit' to end the program: ");
+                string userInput = Console.ReadLine();
+                if (userInput == "quit")
+                {
+                    break;
+                }
+                scripture.HideWords();
+                scripture.Display();
+            }
+
+            Console.WriteLine("Congratulations! You have successfully hidden all words in the scripture.");
+        }
+    }
+
+    class Scripture
+    {
+        private string reference;
+        private string text;
+        private bool[] hiddenWords;
+
+        public Scripture(string reference, string text)
+        {
+            this.reference = reference;
+            this.text = text;
+            this.hiddenWords = Enumerable.Repeat(false, text.Split(' ').Length).ToArray();
+        }
+
+        public bool HasHiddenWords()
+        {
+            return this.hiddenWords.Contains(false);
+        }
+
+        public void HideWords()
+        {
+            Random random = new Random();
+            int hiddenWordIndex = random.Next(0, this.text.Split(' ').Length);
+            this.hiddenWords[hiddenWordIndex] = true;
+        }
+
+        public void Display()
+        {
+            Console.Clear();
+            string[] words = this.text.Split(' ');
+            Console.WriteLine("Scripture: " + this.reference);
+            Console.WriteLine("");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!this.hiddenWords[i])
+                {
+                    Console.Write(words[i] + " ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                }
+            }
+            Console.WriteLine("");
+        }
     }
 }
