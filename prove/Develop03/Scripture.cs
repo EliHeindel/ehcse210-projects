f85409bf941814b85b3b97d1d32fefa3de6b8ea5
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-20 18:24:07 -0700

    Create Scripture.cs

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+
+class Scripture
+{
+    private List<Word> words;
+    private int nextWordIndex;
+
+    public Scripture(string reference, string text)
+    {
+        Reference = reference;
+        words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public string Reference { get; }
+
+    public bool IsFullyHidden => nextWordIndex >= words.Count;
+
+    public string GetHiddenText() => string.Join(" ", words.Select(w => w.IsHidden ? "_" : w.Value));
+
+    public void HideNextWord()
+    {
+        if (!IsFullyHidden)
+        {
+            words[nextWordIndex].Hide();
+            nextWordIndex++;
+        }
+    }
+}
