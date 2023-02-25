a772a3c2c88465cf88ad1d7b8a2b08cdd5fed5cd
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-25 15:27:14 -0700

    Update Activity.cs

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,43 +1,71 @@
 using System;
 
-abstract class Activity
+namespace MindfulnessApp
 {
-    protected string name;
-    protected string description;
-    protected int duration;
-
-    public Activity(string name, string description)
+    public abstract class Activity
     {
-        this.name = name;
-        this.description = description;
-    }
+        protected string name;
+        protected string description;
+        protected int duration;
 
-    protected void Pause(int seconds)
-    {
-        for (int i = seconds; i >= 1; i--)
+        public Activity(string name, string description)
         {
-            Console.Write($"\r{i} ");
-            System.Threading.Thread.Sleep(1000);
-            Console.Write("\r   ");
+            this.name = name;
+            this.description = description;
         }
+
+        public void Start()
+        {
+            Console.WriteLine($"\n{name} Activity");
+            Console.WriteLine(description);
+
+            duration = GetDuration();
+
+            Console.WriteLine($"\nPrepare to begin in 3 seconds.");
+            Countdown(3);
+
+            RunActivity();
+
+            Console.WriteLine($"\nGood job! You have completed the {name} activity for {duration} seconds.");
+            Console.WriteLine("Take some time to relax and clear your mind.");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        protected int GetDuration()
+        {
+            Console.WriteLine("\nPlease enter the duration of the activity in seconds:");
+            string input = Console.ReadLine();
+
+            int duration;
+            if (!int.TryParse(input, out duration) || duration <= 0)
+            {
+                Console.WriteLine("Invalid input. Duration must be a positive integer.");
+                return GetDuration();
+            }
+
+            return duration;
+        }
+
+        protected void Pause(int seconds)
+        {
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write(".");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        protected void Countdown(int seconds)
+        {
+            for (int i = seconds; i > 0; i--)
+            {
+                Console.Write($"{i}...");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine("Go!");
+        }
+
+        protected abstract void RunActivity();
     }
-
-    public void Start()
-    {
-        Console.WriteLine($"Starting {name} activity.");
-        Console.WriteLine(description);
-
-        Console.Write("Enter duration in seconds: ");
-        duration = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Prepare to begin.");
-        Pause(3);
-
-        RunActivity();
-
-        Console.WriteLine($"Good job! You completed the {name} activity for {duration} seconds.");
-        Pause(3);
-    }
-
-    protected abstract void RunActivity();
 }
