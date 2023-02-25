e660e3affa41731cb38e193aeed0d8fe72ca1709
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-25 15:14:26 -0700

    Update BreathingActivity.cs

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -2,16 +2,17 @@ using System;
 
 class BreathingActivity : Activity
 {
-    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.") { }
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.") { }
 
     protected override void RunActivity()
     {
-        for (int i = 0; i < duration; i += 2)
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+        while (DateTime.Now < endTime)
         {
             Console.WriteLine("Breathe in...");
-            Pause(3);
+            Pause(2);
             Console.WriteLine("Breathe out...");
-            Pause(3);
+            Pause(2);
         }
     }
 }
