55c60e845c6f522de795d416814e5152bf92fa9f
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-25 15:01:12 -0700

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Welcome to the Mindfulness app!");
+        Console.WriteLine("Choose an activity:");
+        Console.WriteLine("1. Breathing");
+        Console.WriteLine("2. Reflection");
+        Console.WriteLine("3. Listing");
+
+        int choice = int.Parse(Console.ReadLine());
+
+        Activity activity = null;
+        switch (choice)
+        {
+            case 1:
+                activity = new BreathingActivity();
+                break;
+            case 2:
+                activity = new ReflectionActivity();
+                break;
+            case 3:
+                activity = new ListingActivity();
+                break;
+            default:
+                Console.WriteLine("Invalid choice.");
+                return;
+        }
+
+        activity.Start();
     }
 }
