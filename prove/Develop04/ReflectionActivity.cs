6050160a3ca284e7c3538cb1c003ede60148b9e9
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-25 19:52:01 -0700

    Update ReflectionActivity.cs

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -1,39 +1,24 @@
 using System;
+using System.Threading;
 
 class ReflectionActivity : Activity
 {
-    private List<string> prompts = new List<string>() {
-        "Think of a time when you stood up for someone else.",
-        "Think of a time when you did something really difficult.",
-        "Think of a time when you helped someone in need.",
-        "Think of a time when you did something truly selfless."
-    };
-    private List<string> questions = new List<string>() {
-        "Why was this experience meaningful to you?",
-        "Have you ever done anything like this before?",
-        "How did you get started?",
-        "How did you feel when it was complete?",
-        "What made this time different than other times when you were not as successful?",
-        "What is your favorite thing about this experience?",
-        "What could you learn from this experience that applies to other situations?",
-        "What did you learn about yourself through this experience?",
-        "How can you keep this experience in mind in the future?"
-    };
+    private string[] prompts = {"Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need.", "Think of a time when you did something truly selfless."};
+    private string[] questions = {"Why was this experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?", "What made this time different than other times when you were not as successful?", "What is your favorite thing about this experience?", "What could you learn from this experience that applies to other situations?", "What did you learn about yourself through this experience?", "How can you keep this experience in mind in the future?"};
 
     public ReflectionActivity() : base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.") { }
 
     protected override void RunActivity()
     {
-        DateTime endTime = DateTime.Now.AddSeconds(duration);
-        Random rand = new Random();
-        string prompt = prompts[rand.Next(prompts.Count)];
-        Console.WriteLine(prompt);
-
-        while (DateTime.Now < endTime)
+        int promptIndex = new Random().Next(prompts.Length);
+        Console.WriteLine("Prompt: " + prompts[promptIndex]);
+        Pause(5);
+        foreach (string question in questions)
         {
-            string question = questions[rand.Next(questions.Count)];
-            Console.WriteLine(question);
-            Pause(5);
+            Console.Write(question + " ");
+            Spinner(3);
+            Console.WriteLine();
+            Pause(2);
         }
     }
 }
