585a10077f1d44ca08bfc611b8cae8f40857c8f8
EliHeindel <123850678+EliHeindel@users.noreply.github.com>
2023-02-25 19:54:34 -0700

    Update Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,45 +1,54 @@
 using System;
 
-namespace MindfulnessApp
+namespace MindfulActivities
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bool running = true;
-            while (running)
+            Console.WriteLine("Welcome to Mindful Activities!");
+
+            while (true)
             {
-                Console.WriteLine("Choose an activity:");
+                Console.WriteLine("\nPlease select an activity:");
                 Console.WriteLine("1. Breathing");
                 Console.WriteLine("2. Reflection");
                 Console.WriteLine("3. Listing");
                 Console.WriteLine("4. Quit");
 
-                int choice = int.Parse(Console.ReadLine());
-
-                Activity activity;
-
-                switch (choice)
+                int choice = 0;
+                while (choice < 1 || choice > 4)
                 {
-                    case 1:
-                        activity = new BreathingActivity();
-                        break;
-                    case 2:
-                        activity = new ReflectionActivity();
-                        break;
-                    case 3:
-                        activity = new ListingActivity();
-                        break;
-                    case 4:
-                        running = false;
-                        continue;
-                    default:
-                        Console.WriteLine("Invalid choice");
-                        continue;
+                    Console.Write("Enter your choice: ");
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out choice) == false)
+                    {
+                        choice = 0;
+                    }
                 }
 
-                activity.Start();
+                if (choice == 1)
+                {
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Start();
+                }
+                else if (choice == 2)
+                {
+                    ReflectionActivity reflection = new ReflectionActivity();
+                    reflection.Start();
+                }
+                else if (choice == 3)
+                {
+                    ListingActivity listing = new ListingActivity();
+                    listing.Start();
+                }
+                else
+                {
+                    break;
+                }
             }
+
+            Console.WriteLine("Thank you for using Mindful Activities!");
         }
     }
 }
